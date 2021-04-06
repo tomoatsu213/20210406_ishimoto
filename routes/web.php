87176ed5b8adbf9dda608b35f249866e19df4393
tomoatsu <tomoatsu213@gmail.com>
2021-04-06 19:28:17 +0900

    first-commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//bブログ一覧画面を表示
+Route::get('/', [BlogController::class, 'showList'])->name('blogs');
